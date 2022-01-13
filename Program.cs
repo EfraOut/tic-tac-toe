99cc6ebfa8331892dae6594355524623a60e85c0
Efrain Arturo Gomez Fajardo <gom21023@byui.edu>
2022-01-13 15:06:34 -0700

    Displaying the board on the console

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class ticatactoe
+    {
+        static void Main(string[] args)
+        {
+            string[] board = createBoard();
+            displayBoard(board);
+        }
+
+        static string[] createBoard()
+        {
+            string[] board = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+
+            return board;
+        }
+
+        static void displayBoard(string[] board)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"{board[0]}|{board[1]}|{board[2]}");
+            Console.WriteLine($"{board[3]}|{board[4]}|{board[5]}");
+            Console.WriteLine($"{board[6]}|{board[7]}|{board[8]}");
+            Console.WriteLine();
+        }
+    }
